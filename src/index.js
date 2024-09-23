951c118bcaa869016eea09d74209668db590eed7
Shao-911 <nyanjao@gmail.com>
2024-09-23 18:33:02 +0300

    updated links on html

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "833c0f856af9804ad7bb142f0bot5682";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
